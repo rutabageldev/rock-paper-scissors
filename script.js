51e0af40c824538e1b4498df2c8e480e5280d75a
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-10 17:58:06 -0500

    Built player choice request and validation Created function to request the players choice and validate as a good selection.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello, world!");
+/*
+Start game, round = 1
+For (1 =< round =< 5) {
+    Ask player for rock, paper, or scissors
+    Randomly generate computer choice
+    Determine winner 
+    Winner score +1
+}
+*/
+getPlayerChoice();
+
+function getPlayerChoice() {
+    let goodChoice = false;     //state variable that tracks validity of player's choice
+    let playerChoice = prompt(`Please select one of the following:
+        -Rock
+        -Paper
+        -Scissors`);
+    playerChoice = playerChoice.toLowerCase();
+    while(!goodChoice){
+        goodChoice = validateChoice(playerChoice);
+        console.log("goodChoice: " + goodChoice);
+        console.log("playerChoice: " + playerChoice);
+        if (!goodChoice) {
+            playerChoice = prompt(`Invalid selection, please input one of the following:
+                -Rock
+                -Paper
+                -Scissors`);
+            playerChoice = playerChoice.toLowerCase();    
+        }
+    }
+    return playerChoice;
+}
+
+function validateChoice(choice) {
+    if (choice == 'rock' || choice == 'paper' || choice == 'scissors') {
+        return true;
+    } else {
+        return false;
+    }
+}
