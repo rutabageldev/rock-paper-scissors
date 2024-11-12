adfa8430874d8a0477766eec8e3b76b545f1e63f
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-11 19:05:32 -0500

    Implemented status and score display updates. Added fucntions to update the player and computer choices each round and display the round results on the status bar.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const rockButton = document.querySelector("#rock");
+const computerDisplay = document.querySelector (".computer-choice");
+const newGameButton = document.querySelector(".new-game");
 const paperButton = document.querySelector("#paper");
+const playerDisplay = document.querySelector (".player-choice");
+const rockButton = document.querySelector("#rock");
 const scissorsButton = document.querySelector("#scissors");
+const scoreDisplay = document.querySelector(".score");
+const statusDisplay = document.querySelector(".status-bar");
 
-rockButton.addEventListener("click", playRound);
 paperButton.addEventListener("click", playRound);
+rockButton.addEventListener("click", playRound);
 scissorsButton.addEventListener("click", playRound);
+newGameButton.addEventListener("click", newGame);
 
+let gameState = 'new';
 let scorePlayer = 0;
 let scoreComputer = 0;
 let round = 1;
 let playerChoice = null;
 let computerChoice = null;
 let roundState = null;
-
-/*for (let round = 1; round < 6; round++) {
-    //playerChoice = getPlayerChoice();
-    computerChoice = getComputerChoice();
-    roundState = scoreRound(playerChoice, computerChoice);
-    roundLogging(round, playerChoice, computerChoice, roundState, scorePlayer, scoreComputer);
-}*/
 
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3)) + 1;
@@ -35,36 +35,43 @@ function getComputerChoice() {
     }
 }
 
+function newGame(e) {
+    console.log(e);
+    console.log(e.target.id);
+    gameState = 'new';
+    scorePlayer = 0;
+    scoreComputer = 0;
+    round = 1;
+    roundState = null;
+    playerChoice = null;
+    computerChoice = null;
+    roundLogging(round, playerChoice, computerChoice, roundState, scorePlayer, scoreComputer, gameState);
+    updateInfo();
+
+}
+
 function playRound(e) {
+    switch (gameState) {
+        case ('new'):
+            gameState = 'active';
+            break;
+        case ('active'):
+            break;
+        case ('over'):
+            return;
+    }
     console.log(e);
     console.log(e.target.id);
     playerChoice = e.target.id;
     computerChoice = getComputerChoice();
     roundState = scoreRound(playerChoice, computerChoice);
-    roundLogging(round, playerChoice, computerChoice, roundState, scorePlayer, scoreComputer);
+    roundLogging(round, playerChoice, computerChoice, roundState, scorePlayer, scoreComputer, gameState);
     round++;
-}
-
-/*function getPlayerChoice() {
-    let goodChoice = false;     //state variable that tracks validity of player's choice
-    let playerChoice = prompt(`Please select one of the following:
-        -Rock
-        -Paper
-        -Scissors`);
-    playerChoice = playerChoice.toLowerCase();
-    while(!goodChoice){
-        goodChoice = validateChoice(playerChoice);
-        console.log("goodChoice: " + goodChoice);
-        if (!goodChoice) {
-            playerChoice = prompt(`Invalid selection, please input one of the following:
-                -Rock
-                -Paper
-                -Scissors`);
-            playerChoice = playerChoice.toLowerCase();    
-        }
+    if(scorePlayer == 5 || scoreComputer ==5) {
+        gameState = 'over';
     }
-    return playerChoice;
-}*/
+    updateInfo();
+}  
 
 function roundLogging(round, playerChoice, computerChoice, roundState, scorePlayer, scoreComputer) {
     console.log("round: " + round);
@@ -73,6 +80,7 @@ function roundLogging(round, playerChoice, computerChoice, roundState, scorePlay
     console.log("roundState: " + roundState);
     console.log("scorePlayer: " + scorePlayer);
     console.log("scoreComputer: " + scoreComputer);
+    console.log("gameState: " + gameState);
 }
 
 function scoreRound(playerChoice, computerChoice) {
@@ -114,10 +122,49 @@ function scoreRound(playerChoice, computerChoice) {
     }
 }
 
-function validateChoice(choice) {
-    if (choice == 'rock' || choice == 'paper' || choice == 'scissors') {
-        return true;
-    } else {
-        return false;
+function updateInfo() {
+    // Update score display
+    scoreDisplay.textContent = `${scorePlayer}:${scoreComputer}`;
+    // Update display to show last player and computer choice
+    playerDisplay.textContent = displayUpdate(playerChoice);
+    computerDisplay.textContent = displayUpdate(computerChoice);
+    // Display who won the last round or new game message
+    statusDisplay.textContent = statusUpdate();
+
+
+}
+
+function displayUpdate(choice) {
+    switch(choice) {
+        case('rock'):
+            return 'R';
+        case('paper'):
+            return 'P';
+        case('scissors'):
+            return 'S';
+        case(null):
+            return '?';
+    }
+}
+
+function statusUpdate() {
+    switch (gameState) {
+        case ('over'):
+            if (scorePlayer > scoreComputer) {
+                return "Player wins, game over!"
+            } else {
+                return "Computer wins, game over"
+            }
+        case ('new'):
+            return "Waiting for player's choice";
+        case ('active') :
+            switch (roundState){
+                case ('winPlayer'):
+                    return `Player wins, ${playerChoice} beats ${computerChoice}`
+                case ('winComputer'):
+                    return `Computer wins, ${computerChoice} beats ${playerChoice}`
+                case('tie'):
+                    return `Tie, you both chose ${playerChoice}`
+            }
     }
 }
